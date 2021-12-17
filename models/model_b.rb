6ad5c3e905ab3ab6f520476bf3662f374253fd0f
Joseph Senecal <jasenecal2@gmail.com>
2021-12-17 12:15:18 -0700

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Joe Senecal",
+  "program" => "Back-End Development",
+  "languages" => 3,
+  "years_experience" => 1.5,
+  "is_nerveous" => true,
+  "is_excited" => true,
+  "work_completed" =>  ["all homework", "Professional Development PW", "Back-End Pre-Work"],
+  "work_todo" => ["Mod 0 technical assessment", "Gear-Up", "Final Submissions"]
+}
