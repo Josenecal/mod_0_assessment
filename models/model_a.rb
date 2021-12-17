aa6b3ce6fc89f77978a95a2e25029b1527384c6f
Joseph Senecal <jasenecal2@gmail.com>
2021-12-17 11:58:07 -0700

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  name: "stegosaurus",
+  defense: "thagomizer",
+  survivs_kt: false,
+  lays_eggs: true,
+  spike_count: 4,
+  height: 9.13,
+  diet: ["leaves", "berries", "bark", "shrubs"]
+  predators: ["T-Rex", "velociraptor", "space rocks"]
+}
