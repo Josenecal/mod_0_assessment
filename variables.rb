58add99eefe7c65f1dc7b961187ab5a9c4bcbf31
Joseph Senecal <jasenecal2@gmail.com>
2021-12-17 11:46:03 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+movie = "Die Hard"
+is_entertaining = true
+movie = "Live-Action Cats"
+#is_entertaining = false
+num_4 = 4
+num_6 = 7
+product = num_4 * num_6
+favorite_foods = ["coffee", "meat", "potatoes"]
+favorite_foods.shift
+puts "The variable 'movie' contains #{movie.length} characters"
